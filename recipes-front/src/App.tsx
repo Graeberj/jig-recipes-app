a2851087c87b629048813135b4e7923db9915a39
Graeberj <graeberj88@gmail.com>
2024-08-22 11:55:26 -0700

    initialize front end and back end

---
+++ recipes-front/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/hello/")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
